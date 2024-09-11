0326f997aef38a85e65397a7740822a8d1b2c70a
vladovidiu <vlad.tutunea@shopify.com>
2024-09-11 11:40:38 +0100

    add sorbet

---
+++ lua/plugins/sorbet.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        sorbet = {},
+      },
+    },
+    setup = {
+      sorbet = function()
+        local function sorbet_root_pattern(...)
+          local patterns = { "sorbet/config" }
+          return require("lspconfig.util").root_pattern(unpack(patterns))(...)
+        end
+        require("lspconfig").sorbet.setup({
+          cmd = { "srb", "tc", "--lsp" },
+          filetypes = { "ruby" },
+          root_dir = function(fname)
+            return sorbet_root_pattern(fname)
+          end,
+        })
+      end,
+    },
+  },
+}
