51cead04b167934cae65e72f9a917f011b5ddd39
vladovidiu <vlad.tutunea@shopify.com>
2024-09-24 11:45:06 +0100

    add obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,15 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    workspaces = {
+      {
+        name = "neuravault",
+        path = "~/Documents/neuravault/",
+      },
+    },
+  },
+}
