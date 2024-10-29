712fc6152a5a9a295e14931eff299d332108990a
vladovidiu <vlad.tutunea@shopify.com>
2024-10-29 12:39:23 +0000

    update avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,10 +5,40 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
-    provider = "claude",
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
+  config = function()
+    local openai = require("avante.providers.openai")
+    -- local anthropic = require("avante.providers.anthropic")
+
+    -- Get the hostname
+    local hostname = io.popen("hostname"):read("*l")
+
+    local config
+    if hostname:match("quasar") then
+      -- Work configuration
+      config = {
+        provider = "shopify-ai",
+        vendors = {
+          ["shopify-ai"] = {
+            endpoint = "https://proxy.shopify.ai/v3/v1",
+            model = "anthropic:claude-3-5-sonnet",
+            api_key_name = "cmd:openai_key cat",
+            parse_curl_args = openai.parse_curl_args,
+            parse_response_data = openai.parse_response,
+          },
+        },
+      }
+    else
+      -- Personal configuration
+      config = {
+        provider = "claude",
+      }
+    end
+
+    require("avante").setup(config)
+  end,
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
@@ -16,7 +46,7 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "echasnovski/mini.icons",
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "zbirenbaum/copilot.lua", -- for providers=''
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
