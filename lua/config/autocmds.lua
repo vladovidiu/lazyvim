1d09a70caaf6cc91477e9650c9cb299007d0976c
vladovidiu <vlad.tutunea@shopify.com>
2025-02-12 13:56:03 +0000

    remove obsolete autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,3 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_user_command("RefreshAiProxyKey", ":lua require('local-plugins.proxy_key').get_proxy_key()", {})
