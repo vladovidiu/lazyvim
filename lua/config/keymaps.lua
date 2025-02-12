af53c554cb25b71e7866b18e6961437b21f7b251
vladovidiu <vlad.tutunea@shopify.com>
2025-02-12 14:05:05 +0000

    add snacks.terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = LazyVim.safe_keymap_set
+local snacks = require("snacks")
 
-local lazyterm = function()
-  LazyVim.terminal(nil, { cwd = LazyVim.root(), border = "rounded" })
+--- Map a key combination to a command
+---@param modes string|string[]: The mode(s) to map the key combination to
+---@param lhs string: The key combination to map
+---@param rhs string|function: The command to run when the key combination is pressed
+---@param opts table: Options to pass to the keymap
+local map = function(modes, lhs, rhs, opts)
+  local options = { silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  if type(modes) == "string" then
+    modes = { modes }
+  end
+  for _, mode in ipairs(modes) do
+    vim.keymap.set(mode, lhs, rhs, options)
+  end
 end
-map("n", "<c-\\>", lazyterm, { desc = "Terminal (Root Dir)" })
-map("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+
+map({ "n", "t" }, "<C-\\>", function()
+  snacks.terminal()
+end, { desc = "Toggle Terminal" })
